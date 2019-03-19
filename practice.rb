f3f8070bdc7750402b1a198200fe760fb0e4c594
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-19 22:45:58 +0700

    fix robocop recommend

--- practice.rb
+++ practice.rb
@@ -1,9 +1,6 @@
-numbers = Array[1,5,3,7]
+numbers = Array[1, 5, 3, 7]
 
 print numbers.sort
 
 # to run the program ; ruby filename.rb
-# Note : add .stickler.yml to the master 
-
-
-
+# Note : add .stickler.yml to the master
