3afc8afc54a1598676955c0ce8be875e5a477a1e
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-19 22:41:25 +0700

    add spaces

--- practice.rb
+++ practice.rb
@@ -2,8 +2,8 @@ numbers = Array[1,5,3,7]
 
 print numbers.sort
 
-#to run the program ; ruby filename.rb
-#Note : add .stickler.yml to the master branch before creating development branch
+# to run the program ; ruby filename.rb
+# Note : add .stickler.yml to the master branch before creating development branch
 
 
 
