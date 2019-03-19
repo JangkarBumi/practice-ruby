91ba593656f22d23f98f2a4dc77c605576298699
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-19 22:40:12 +0700

    remove block comments

--- practice.rb
+++ practice.rb
@@ -2,25 +2,8 @@ numbers = Array[1,5,3,7]
 
 print numbers.sort
 
-=begin
-to run the program
+#to run the program ; ruby filename.rb
+#Note : add .stickler.yml to the master branch before creating development branch
 
-ruby filename.rb
-=end
 
-=begin
-Todo
 
-1.Learn about Rubocop
-2.Learn about lib
-3.Learn about specs
-4.Learn about linter
-5.Learn about test
-
-=end
-
-=begin
-
-Note : add .stickler.yml to the master branch
-
-=end
