afe156464af1cf959a64e49ae065115de41d0cb5
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-19 22:07:38 +0700

    notes

--- practice.rb
+++ practice.rb
@@ -1,1 +1,20 @@
-puts 'test'
+numbers = Array[1,5,3,7]
+
+print numbers.sort
+
+=begin
+to run the program
+
+ruby filename.rb
+=end
+
+=begin
+Todo
+
+1.Learn about Rubocop
+2.Learn about lib
+3.Learn about specs
+4.Learn about linter
+5.Learn about test
+
+=end
